eb8dea2d6f932f97bc525541b973c13b4096beb6
Typosphere <67193118+Typosphere@users.noreply.github.com>
2021-06-09 17:35:34 +0200

    Add files via upload

---
+++ Opdracht-gebruik if-else statement/index.js
@@ -0,0 +1,23 @@
+const age = 14; //expression
+
+if (age >= 18){
+    console.log ("Welcome");
+
+} else if (age < 18){
+    console.log ("Sorry, we ask you to go outside, thank you.");
+} else
+
+const isFemale = true;
+
+if (isFemale){
+    console.log ("Welcome, it's ladynight");
+} else (isFemale)
+    console.log ("Sorry, only ladies tonight");
+
+
+const driverStatus = "BoB";
+
+if (driverStatus){
+    console.log ("You are BoB");
+} else 
+    console.log ("You are not BoB, you can't drive");
